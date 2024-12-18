378664d5f9eb46290596079ba808d937418e4246
sabbir00921 <sabbir00921@gmail.com>
2024-12-18 15:54:13 +0600

    Voter eligibility checking user input solve

--- Voter elegiblelity check/js/main.js
+++ Voter elegiblelity check/js/main.js
@@ -1,10 +1,35 @@
 // multiple value array er modhye store korbe then sort korbe.
-function myFunction (){
-    const arr =[];
-    for( let i=0 ; i< 5; i++){
-        const number = +prompt ("Enter the number")
-        arr.push(number);
+
+function myFunction() {
+    const arr = [];
+    for (let i = 0; i < 5; i++) {
+        const iniputAge = +prompt("give voters age")
+        const number = iniputAge + (Math.random() * 20)// Create a random value from user input
+        arr.push(Math.round(number));
     }
-    console.log(arr)
+    return arr;
 }
-myFunction()
+const result = myFunction()
+console.log(result)
+
+let unAble = 0
+votingEligble = (result, unAble) => {
+    const able = []
+    const notAble = []
+    for (let i = 0; i < result.length; i++) {
+
+        if (result[i] >= 18) {
+            console.log(`${result[i]} is ready for voating`)
+            able.push(result[i])
+        }
+        else {
+            console.log(`${result[i]} is not ready for voating`)
+            notAble.push(result[i])
+        }
+
+    }
+    return [able, notAble];
+}
+const eligibleVoterarr = votingEligble(result, unAble)
+console.log(`Eligible voter age ${eligibleVoterarr[0]} \nNot eligible voter age ${eligibleVoterarr[1]} \n`)
+console.log(`${eligibleVoterarr[0].length} person are eligible gor voting`)
